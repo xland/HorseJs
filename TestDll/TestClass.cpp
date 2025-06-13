128acd5dce4b8bae4788ec7c1bc00498ca64880c
liulun <xland@live.cn>
2025-06-13 20:31:26 +0800

    dll test code

--- TestDll/TestClass.cpp
+++ TestDll/TestClass.cpp
@@ -1,4 +1,4 @@
-#include "pch.h"
+﻿#include "pch.h"
 #include <combaseapi.h>
 #include <string>
 #include <thread>
@@ -6,6 +6,7 @@
 #include "TestClass.h"
 
 static EventCB _eventCB{nullptr};
+static std::atomic<bool> stopFlag{ false };   // 停止标志
 
 const char* joinStr(const char* param) {
     std::string str{ param };
@@ -22,16 +23,16 @@ const char* joinStr(const char* param) {
 const char* regEvent(const char* eventName, EventCB eventCB)
 {
     _eventCB = eventCB;
-    std::string eName(eventName);
-    std::string tarName("customEvent");
-    if (eName == tarName) {
+    if (std::string(eventName) == "customEvent") {
         //99%的情况下，事件都是在新线程内执行的
         std::jthread worker{ []() {
-            for (size_t i = 0; i < 88; i++)
+            for (size_t i = 0; i < 88 && !stopFlag; i++)
             {
-                std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-                //参数占用的内存，需要Dll开发者负责释放
-                _eventCB("customEvent", "customEvent data"); 
+                std::this_thread::sleep_for(std::chrono::milliseconds(6000));
+                if (_eventCB && !stopFlag) {
+                    //参数占用的内存，需要Dll开发者负责释放
+                    _eventCB("customEvent", "customEvent data");
+                }
             }
         } };
         worker.detach();
@@ -42,5 +43,10 @@ const char* regEvent(const char* eventName, EventCB eventCB)
 const char* unregEvent(const char* eventName)
 {
     //dll 开发者负责停止event线程
+    if (std::string(eventName) == "customEvent") {
+        stopFlag = true;
+        _eventCB = nullptr; // 清理回调
+        return "ok";
+    }
     return nullptr;
 }
